42f9c3ac6d4e24d03e555d8e509f9d759166ea09
D-AIRY <admin@ds-servers.com>
2018-08-26 00:31:39 +0300

    Added concurrent queue class

---
+++ ConcurrentQueue.h
@@ -0,0 +1,80 @@
+#ifndef __ConcurrentQueue_H
+#define __ConcurrentQueue_H
+
+#include <mutex>
+#include <condition_variable>
+#include <queue>
+#include <thread>
+
+template<typename T> class CConcurrentQueue
+{
+	typedef std::unique_lock<std::mutex> ScopedLock;
+
+public:
+	bool empty() const
+	{
+		ScopedLock lock(m_mutex);
+		return(m_queue.empty());
+	}
+
+	bool tryPop(T &out)
+	{
+		ScopedLock lock(m_mutex);
+
+		if(!m_queue.empty())
+		{
+			out = m_queue.front();
+			m_queue.pop();
+
+			return(true);
+		}
+
+		return(false);
+	}
+
+	T pop()
+	{
+		ScopedLock lock(m_mutex);
+
+		while(m_queue.empty())
+		{
+			m_condVar.wait(lock);
+		}
+
+		T res = m_queue.front();
+		m_queue.pop();
+
+		return(res);
+	}
+
+	void push(const T &value)
+	{
+		{
+			ScopedLock lock(m_mutex);
+
+			m_queue.push(value);
+		}
+
+		m_condVar.notify_one();
+	}
+
+	std::size_t size() const
+	{
+		ScopedLock lock(m_mutex);
+
+		return(mQueue.size());
+	}
+
+	void clear()
+	{
+		ScopedLock lock(m_mutex);
+		std::queue<T>().swap(m_queue);
+	}
+
+private:
+	std::queue<T> m_queue;
+	mutable std::mutex m_mutex;
+	std::condition_variable m_condVar;
+};
+
+#endif
