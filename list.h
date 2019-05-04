d9256657ef79baf0429ac210a95ffe26cc906e69
Dokunov <vanya619@list.ru>
2019-05-04 22:01:11 +0300

    style refactor, add operator =&& and operator =

--- list.h
+++ list.h
@@ -9,136 +9,156 @@ class List final
 private:
     struct chunk
     {
-        Type obj;
-        chunk* next = nullptr;
-        chunk* prev = nullptr;
+        Type m_obj;
+        chunk *m_next = nullptr;
+        chunk *m_prev = nullptr;
     };
 
-    chunk* pLast;
-    chunk* pHead;
-    int size;
+    chunk *m_pLast = nullptr;
+    chunk *m_pHead = nullptr;
+    int m_size = 0;
 
-    void fillHead(chunk* iterator)
+    void fillHead(chunk *iterator)
     {
-        pLast = iterator;
+        m_pLast = iterator;
 
-        while (iterator->next)
+        while (iterator->m_next)
         {
-            iterator = iterator->next;
+            iterator = iterator->m_next;
         }
 
-        pHead = iterator;
+        m_pHead = iterator;
     }
 
 public:
     //Создает пустой список
-    explicit List() : size(0), pHead(nullptr), pLast(nullptr)
-    { }
+    List() = default;
 
-    explicit List(const std::initializer_list<Type> & list) : size(list.size())
+    explicit List(const std::initializer_list<Type> &list) : m_size(list.m_size())
     {
-        chunk* iterator = nullptr;
+        chunk *iterator = nullptr;
 
         for (const auto &x : list)
         {
-            chunk* newObj = new chunk;
+            chunk *newObj = new chunk;
 
             if (iterator)
             {
-                iterator->prev = newObj;
-                newObj->next = iterator;
+                iterator->m_prev = newObj;
+                newObj->m_next = iterator;
             }
-            newObj->obj = x;
+            newObj->m_obj = x;
             iterator = newObj;
         }
 
         fillHead(iterator);
     }
 
-    List(const List& other) : size(other.size)
+    List(const List &other)
     {
-        if (other.size != 0)
+        operator=(other);
+    }
+
+    List(List &&other)
+    {
+        operator=((List&&)other);
+    }
+
+    const List &operator=(const List &other)
+    {
+        m_size = other.m_size;
+
+        if (other.m_size != 0)
         {
-            chunk* otherLast = other.pLast;
-            chunk* iterator = nullptr;
+            chunk *otherLast = other.m_pLast;
+            chunk *iterator = nullptr;
 
             while (otherLast)
             {
-                chunk* newObj = new chunk;
-                newObj->obj = otherLast->obj;
+                chunk *newObj = new chunk;
+                newObj->m_obj = otherLast->m_obj;
 
                 if (iterator)
                 {
-                    iterator->prev = newObj;
-                    newObj->next = iterator;
+                    iterator->m_prev = newObj;
+                    newObj->m_next = iterator;
                 }
                 iterator = newObj;
 
-                otherLast = otherLast->next;
+                otherLast = otherLast->m_next;
             }
 
             fillHead(iterator);
         }
+        return this;
     }
 
-    List(List&& other)
-        : pLast(other.pLast), pHead(other.pHead), size(other.size)
+    const List &operator=(const List &&other)
     {
-        other.pLast = nullptr;
-        other.pHead = nullptr;
-        other.size = 0;
+        m_pLast = other.m_pLast;
+        m_pHead = other.m_pHead;
+        m_size = other.m_size;
+
+        other.m_pLast = nullptr;
+        other.m_pHead = nullptr;
+        other.m_size = 0;
     }
 
     //!true если в list пустой
     bool empty() const
     {
-        return size == 0;
+        return m_size == 0;
     }
 
-    int size() const
+    int m_size() const
     {
-        return size;
+        return m_size;
     }
 
     void clear()
     {
-        if (!pHead)
+        if (!m_pHead)
         {
             return;
         }
 
         chunk *iterator = nullptr;
 
-        while (pHead->prev)
+        while (m_pHead->m_prev)
         {
-            iterator = pHead;
-            pHead = pHead->prev;
+            iterator = m_pHead;
+            m_pHead = m_pHead->m_prev;
             delete iterator;
         }
 
-        delete pHead;
+        delete m_pHead;
 
-        pHead = nullptr;
-        pLast = nullptr;
-        size = 0;
+        m_pHead = nullptr;
+        m_pLast = nullptr;
+        m_size = 0;
     }
 
-    void push_front(Type & obj)
+    void push_front(const Type &m_obj)
     {
-        chunk* newObj = new chunk();
-        newObj->obj = obj;
-        newObj->prev = pHead;
-        pHead->next = newObj;
-        pHead = newObj;
+        chunk *newObj = new chunk();
+        newObj->m_obj = m_obj;
+        newObj->m_prev = m_pHead;
+        m_pHead->m_next = newObj;
+        m_pHead = newObj;
     }
 
-    void push_back(Type & obj)
+    void push_back(const Type &m_obj)
     {
-        chunk* newObj = new chunk();
-        newObj->obj = obj;
-        newObj->next = pLast;
-        pLast->prev = newObj;
-        plast = newObj;
+        chunk *newObj = new chunk();
+        newObj->m_obj = m_obj;
+        newObj->m_next = m_pLast;
+        m_pLast->m_prev = newObj;
+        m_pLast = newObj;
+    }
+
+    ~List()
+    {
+        clear();
     }
 };
 
